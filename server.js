b0bb413734b0f5819587d50266be75eb2a1f1158
Variiiik <klaus.varik@khk.ee>
2025-06-19 23:14:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,97 +11,44 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-//const client = new MongoClient(process.env.MONGO_URI);
-//let db, drivers, driverDetails;
+const client = new MongoClient(process.env.MONGO_URI);
+let db, drivers, driverDetails;
 
-const proDrivers = [
-  { competitorId: "pro-1", competitorName: "JANUŠAUSKAITĖ Sandra", nationality: "LTU", car: "BMW F22", competitionClass: "PRO" },
-  { competitorId: "pro-2", competitorName: "FABISIAK Dariusz", nationality: "POL", car: "Nissan 200SX S14", competitionClass: "PRO" },
-  { competitorId: "pro-3", competitorName: "WARTAŁOWICZ Marek", nationality: "POL", car: "Nissan PS13", competitionClass: "PRO" },
-  { competitorId: "pro-4", competitorName: "MLADENOV Samuel", nationality: "SVK", car: "BMW E92", competitionClass: "PRO" },
-  { competitorId: "pro-5", competitorName: "FIJAŁ Aleksandra", nationality: "POL", car: "Nissan S13 200SX", competitionClass: "PRO" },
-  { competitorId: "pro-6", competitorName: "MERILOO Graig", nationality: "EST", car: "BMW Presision next gen", competitionClass: "PRO" },
-  { competitorId: "pro-7", competitorName: "WAAGAARD Joachim", nationality: "NOR", car: "BMW M4", competitionClass: "PRO" },
-  { competitorId: "pro-8", competitorName: "KUR Dominik", nationality: "POL", car: "Opel GT", competitionClass: "PRO" },
-  { competitorId: "pro-9", competitorName: "HEIDRICH Tim", nationality: "POL", car: "Mercedes Benz", competitionClass: "PRO" },
-  { competitorId: "pro-10", competitorName: "OLEJNIK Jacek", nationality: "POL", car: "BMW E92", competitionClass: "PRO" },
-  { competitorId: "pro-11", competitorName: "ZAKRZEWSKI Mikołaj", nationality: "POL", car: "Nissan Silvia S15", competitionClass: "PRO" },
-  { competitorId: "pro-12", competitorName: "STOLARSKI Bartosz", nationality: "POL", car: "Nissan 200SX S15", competitionClass: "PRO" },
-  { competitorId: "pro-13", competitorName: "ERLANDSSON Christian", nationality: "SWE", car: "Subaru BRZ", competitionClass: "PRO" },
-  { competitorId: "pro-14", competitorName: "PILARCZYK Karolina", nationality: "POL", car: "Nissan 200SX S15", competitionClass: "PRO" },
-  { competitorId: "pro-15", competitorName: "JARKIEWICZ Maciej", nationality: "POL", car: "Nissan S14", competitionClass: "PRO" },
-  { competitorId: "pro-16", competitorName: "UTASI Péter", nationality: "HUN", car: "BMW F22", competitionClass: "PRO" },
-  { competitorId: "pro-17", competitorName: "KOZŁOWSKI Piotr", nationality: "POL", car: "Nissan Silvia S15", competitionClass: "PRO" },
-  { competitorId: "pro-18", competitorName: "STAŃCZAK Piotr", nationality: "POL", car: "BMW E82", competitionClass: "PRO" },
-  { competitorId: "pro-19", competitorName: "PRZYGOŃSKI Jakub", nationality: "POL", car: "Toyota GR86", competitionClass: "PRO" },
-  { competitorId: "pro-20", competitorName: "FOJTÍK Michal", nationality: "CZE", car: "BMW M3 E46 V8 Turbo", competitionClass: "PRO" },
-  { competitorId: "pro-21", competitorName: "ALHARBALI URIAS Nasser", nationality: "SLV", car: "Nissan S13", competitionClass: "PRO" },
-  { competitorId: "pro-22", competitorName: "SALMRE Kristjan", nationality: "EST", car: "BMW 1", competitionClass: "PRO" },
-  { competitorId: "pro-23", competitorName: "WROBEL Kevin", nationality: "POL", car: "BMW E36", competitionClass: "PRO" },
-  { competitorId: "pro-24", competitorName: "JÓŹWIAK Krzysztof", nationality: "POL", car: "BMW F22", competitionClass: "PRO" },
-  { competitorId: "pro-25", competitorName: "SHEVELOV Valerii", nationality: "UKR", car: "Nissan S13", competitionClass: "PRO" },
-  { competitorId: "pro-26", competitorName: "MICHALCZUK Orest", nationality: "POL", car: "Nissan 200SX", competitionClass: "PRO" },
-  { competitorId: "pro-27", competitorName: "DOROZIŃSKI Fabian", nationality: "ISL", car: "Nissan S14 200SX", competitionClass: "PRO" },
-  { competitorId: "pro-28", competitorName: "DRUZSIN Matyas", nationality: "HUN", car: "BMW M104 Turbo", competitionClass: "PRO" },
-  { competitorId: "pro-29", competitorName: "WETTEMARK Viktor", nationality: "SWE", car: "Nissan S14.9", competitionClass: "PRO" },
-  { competitorId: "pro-30", competitorName: "KRÓL Jakub", nationality: "POL", car: "Nissan 200SX S14", competitionClass: "PRO" },
-  { competitorId: "pro-31", competitorName: "NEJMAN Marcin", nationality: "POL", car: "BMW F22", competitionClass: "PRO" },
-  { competitorId: "pro-32", competitorName: "HORECKI Kamil", nationality: "POL", car: "BMW E82", competitionClass: "PRO" },
-  { competitorId: "pro-33", competitorName: "KADEŘÁBEK Zdeněk", nationality: "CZE", car: "BMW E46", competitionClass: "PRO" },
-  { competitorId: "pro-34", competitorName: "ANDERSSON Viktor", nationality: "SWE", car: "Toyota GT86", competitionClass: "PRO" },
-  { competitorId: "pro-35", competitorName: "SUSKI Mateusz", nationality: "POL", car: "Toyota Supra", competitionClass: "PRO" },
-  { competitorId: "pro-36", competitorName: "WILCZEK Łukasz", nationality: "POL", car: "BMW M3 E46", competitionClass: "PRO" },
-  { competitorId: "pro-37", competitorName: "HAVRYLENKO Artur", nationality: "UKR", car: "Toyota GT86", competitionClass: "PRO" },
-  { competitorId: "pro-38", competitorName: "BERTRAND Denis", nationality: "BEL", car: "BMW E46", competitionClass: "PRO" },
-  { competitorId: "pro-39", competitorName: "HAMWY Michel", nationality: "SWE", car: "Nissan S14", competitionClass: "PRO" },
-  { competitorId: "pro-40", competitorName: "HUDECEK Pavel", nationality: "CZE", car: "Nissan", competitionClass: "PRO" },
-  { competitorId: "pro-41", competitorName: "KONEČNÝ Jan", nationality: "CZE", car: "BME E36", competitionClass: "PRO" },
-  { competitorId: "pro-42", competitorName: "FIJAŁ Mateusz", nationality: "POL", car: "Toyota GT86", competitionClass: "PRO" },
-  { competitorId: "pro-43", competitorName: "HOMAN Zsolt", nationality: "HUN", car: "Nissan S13", competitionClass: "PRO" },
-  { competitorId: "pro-44", competitorName: "ANDREASEN Mads", nationality: "DNK", car: "BMW M3", competitionClass: "PRO" },
-  { competitorId: "pro-45", competitorName: "MAGYAR Tamás", nationality: "HUN", car: "Mercedes E190", competitionClass: "PRO" },
-  { competitorId: "pro-46", competitorName: "PAWLONKA Krzysztof", nationality: "POL", car: "Nissan S15", competitionClass: "PRO" },
-  { competitorId: "pro-47", competitorName: "KOSZYCARZ Krzysztof", nationality: "POL", car: "BMW F22", competitionClass: "PRO" },
-  { competitorId: "pro-48", competitorName: "REICHERT Michal", nationality: "CZE", car: "BMW E36", competitionClass: "PRO" },
-  { competitorId: "pro-49", competitorName: "TASIEMSKI Łukasz", nationality: "POL", car: "Nissan 200SX", competitionClass: "PRO" },
-  { competitorId: "pro-50", competitorName: "SOUHRADA Lukáš", nationality: "CZE", car: "BMW E92", competitionClass: "PRO" }
-];
+async function connectDB() {
+  await client.connect();
+  db = client.db('spotter');
+  drivers = db.collection('drivers');
+  driverDetails = db.collection('driverDetails');
+  await drivers.createIndex({ competitorId: 1 }, { unique: true });
+  await driverDetails.createIndex({ competitorId: 1 }, { unique: true });
+  console.log('🟢 MongoDB ühendatud');
+}
 
-//async function connectDB() {
-//  await client.connect();
-//  db = client.db('spotter');
-//  drivers = db.collection('drivers');
-//  driverDetails = db.collection('driverDetails');
-//  await drivers.createIndex({ competitorId: 1 }, { unique: true });
-//  await driverDetails.createIndex({ competitorId: 1 }, { unique: true });
-//  console.log('🟢 MongoDB ühendatud');
-//}
+app.get('/api/drivers', async (req, res) => {
+  try {
+    const list = await drivers.aggregate([
+      {
+        $lookup: {
+          from: 'driverDetails',
+          localField: 'competitorId',
+          foreignField: 'competitorId',
+          as: 'details'
+        }
+      },
+      {
+        $unwind: {
+          path: '$details',
+          preserveNullAndEmptyArrays: true
+        }
+      }
+    ]).toArray();
 
-//app.get('/api/drivers', async (req, res) => {
-//  try {
-//    const list = await drivers.aggregate([
-//      {
-//        $lookup: {
-//          from: 'driverDetails',
-//          localField: 'competitorId',
-//          foreignField: 'competitorId',
-//          as: 'details'
-//        }
-//      },
-//      {
-//        $unwind: {
-//          path: '$details',
-//          preserveNullAndEmptyArrays: true
-//        }
-//      }
-//    ]).toArray();
-
-//    res.json(list);
-//  } catch (err) {
-//    console.error('❌ Viga sõitjate laadimisel:', err);
-//    res.status(500).send('Laadimine ebaõnnestus');
-//  }
-//});
+    res.json(list);
+  } catch (err) {
+    console.error('❌ Viga sõitjate laadimisel:', err);
+    res.status(500).send('Laadimine ebaõnnestus');
+  }
+});
 app.get('/api/drivers', (req, res) => {
   res.json(proDrivers);
 });
@@ -295,6 +242,6 @@ app.patch('/api/drivers/:id/note', async (req, res) => {
 
 
 
-//connectDB().then(() => {
+connectDB().then(() => {
   app.listen(port, () => console.log(`🚀 Server käivitatud: http://localhost:${port}`));
-//});
+});
